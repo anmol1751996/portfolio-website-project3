7cb78e9eaf891e461b8455865cf15993f9e9f95c
anmol1751996 <135967161+anmol1751996@users.noreply.github.com>
2023-06-23 10:10:11 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import me from "../assets/logo.png";
+
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillYoutube,
+  AiOutlineArrowUp,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <img
+          src={me}
+          alt="Founder"
+        />
+
+        <h2>Anmol Singh</h2>
+        <p>Motivation is temporary, but discipline last forever.</p>
+      </div>
+
+      <aside>
+        <h2>Social Media</h2>
+
+        <article>
+          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+            <AiFillYoutube />
+          </a>
+          <a href="https://instagram.com/meabhisingh" target={"blank"}>
+            <AiFillInstagram />
+          </a>
+          <a href="" target={"blank"}>
+            <AiFillGithub />
+          </a>
+        </article>
+      </aside>
+      <a href="#home">
+        <AiOutlineArrowUp />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
